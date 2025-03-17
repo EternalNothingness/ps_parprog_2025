0960ef0c59b07cc14d13058346204755e77402ca
Patrick Wintner <p.wintner@aon.at>
2025-03-17 06:59:26 +0100

    added test results

--- 02/src/main.sh
+++ 02/src/main.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Usage: ./main.sh [.sh]
+rm results_flag_O?.log
 for ((i=0;i<=3;++i)); do
 	make clean
 	make OPT="-O"$i
 	for((j=0;j<=2;++j)); do
-		echo "Trial Nr "$j
-		./execall$1 $(nproc --all) "slow" "medium" "fast" > "result_flag_O"$i"_trial"$j
+		echo "flag O"$i", trial "$j
+		./execall$1 $(nproc --all) "slow" "medium" "fast" >> "results_flag_O"$i".log"
+		echo "" >> "results_flag_O"$i".log" #newline
 	done
 done
 make clean
