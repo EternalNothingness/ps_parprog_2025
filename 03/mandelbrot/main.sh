c1a31cf472872ee1a927b0d4ea54f7fd9e08553f
Patrick Wintner <p.wintner@aon.at>
2025-03-24 11:00:21 +0100

    changed main.sh

--- 03/mandelbrot/main.sh
+++ 03/mandelbrot/main.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Usage: ./main.sh
+MEASUREMENT_RESULTS=mandelbrot_measurements.log
+PROCESSED_RESULTS=mandelbrot_processed.log
 make
-rm results_mandelbrot.log
+rm $MEASUREMENT_RESULTS $PROCESSED_RESULTS
 for ((i=0;i<=2;++i)); do
-	/bin/time -a -o "results_mandelbrot.log" ./mandelbrot
+	/bin/time -f "%e" -a -o $MEASUREMENT_RESULTS ./mandelbrot
 done
+./process_results $MEASUREMENT_RESULTS $PROCESSED_RESULTS
 make clean
