c274bc327f0d52979bf9e1e7e7b25171add5eff5
EternalNothingness <p.wintner@aon.at>
2025-03-24 23:44:01 +0100

    fixed bug

--- 03/false_sharing/main_perf.sh
+++ 03/false_sharing/main_perf.sh
@@ -4,7 +4,7 @@ export OMP_NUM_THREADS=6
 export GOMP_CPU_AFFINITY=0,1,2,3,4,5
 echo "running with "$OMP_NUM_THREADS" Threads on cores "$GOMP_CPU_AFFINITY
 perf stat ./false_sharing $PROBLEM_SIZE
-perf stat./false_sharing_2 $PROBLEM_SIZE
+perf stat ./false_sharing_2 $PROBLEM_SIZE
 
 export GOMP_CPU_AFFINITY=0,1,2,6,7,8
 echo "running with "$OMP_NUM_THREADS" Threads on cores "$GOMP_CPU_AFFINITY
