6b5b5d7b67d6548bf961dc009bc2e84568a42e8c
Patrick Wintner <p.wintner@aon.at>
2025-03-24 09:04:17 +0100

    implemented calc_mandelbrot

--- 03/mandelbrot/mandelbrot.c
+++ 03/mandelbrot/mandelbrot.c
@@ -13,7 +13,23 @@
 #define MAX_ITER 10000
 
 void calc_mandelbrot(uint8_t image[Y][X]) {
-	// TODO: Write your code here!
+	for(size_t i=0; i<Y; ++i) {
+		for(size_t j=0; j<X; ++j) {
+			double x=0;
+			double y=0;
+			double cx=(double)j/(X-1)*3.5-2.5; // scale j to [-2.5, 1]
+			double cy=(double)i/(Y-1)*2-1; // scale i to [-1. 1]
+			size_t iteration=0;
+			while(x*x+y*y<2*2 && iteration<MAX_ITER) {
+				double x_tmp=x*x-y*y+cx;
+				y=2*x*y+cy;
+				x=x_tmp;
+				iteration=iteration+1;
+			}
+			char norm_iteration=iteration*255/(MAX_ITER); // scale iteration to [0,255]
+			image[i][j]=norm_iteration;
+		}
+	}
 }
 
 int main() {
