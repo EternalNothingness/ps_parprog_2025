2f808330e1faa1850a2e8dcf944218d0801b347f
Patrick Wintner <p.wintner@aon.at>
2025-04-27 14:06:18 +0200

    minor fix

--- 06/ex1/main.sh
+++ 06/ex1/main.sh
@@ -6,14 +6,13 @@ make
 for i in {1,4,8,12} 
 do
 	export OMP_NUM_THREADS=$i
-	sbatch --output=$1".log" job.sh $1 $N_SAMPLES
+	sbatch --output=$i"_"$1".log" job.sh $1 $N_SAMPLES
 	until [ -f $1".log" ] # wait until file exists
 	do
 		sleep 1
 	done
-	cat $1".log"
 	rm $i"_"$1".tex"
 	./toTex $1".log" $i"_"$1".tex" $N_SAMPLES
-	rm $1".log"
+	#rm $1".log"
 done
 make clean
