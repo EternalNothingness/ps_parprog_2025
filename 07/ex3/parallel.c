9a1408185de2f9aa02e5925f40f7df8a97f76e20
Patrick Wintner <p.wintner@aon.at>
2025-05-05 13:46:53 +0200

    finished s07/ex3

--- 07/ex3/parallel.c
+++ 07/ex3/parallel.c
@@ -1,5 +1,13 @@
-for (int i = 0; i < 4; ++i) {
+#pragma omp parallel for schedule(static)
+for (int i = 0; i < 2; ++i) {
 	for (int j = 1; j < 4; ++j) {
 		a[i + 2][j - 1] = b * a[i][j] + 4;
+	}
+}
+
+#pragma omp parallel for schedule(static)
+for(int i = 2; i < 4; ++i) {
+	for (int j = 1; j < 4; ++j) {
+		a[i+2][j-1] = b * a[i][j] + 4;
 	}
 }
