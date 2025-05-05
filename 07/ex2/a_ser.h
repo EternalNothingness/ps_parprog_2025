305f6471fac1ee318ba9939eba733d612a5c18a5
Patrick Wintner <p.wintner@aon.at>
2025-05-05 12:04:19 +0200

    updated benchmark.sh, a_par.h and a_ser.h

--- 07/ex2/a_ser.h
+++ 07/ex2/a_ser.h
@@ -1,10 +1,10 @@
 #ifndef A_SER_H
 #define A_SER_H
 
-double factor = 1;
+double factor = 1; // S1
 
 for (int i=0; i < n; i++) {
-	x[i] = factor * y[i];
-	factor = factor / 2;
+	x[i] = factor * y[i]; // S2
+	factor = factor / 2; // S3
 }
 #endif
--- 07/ex2/benchmark.sh
+++ 07/ex2/benchmark.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Usage: ./benchmark.sh <executable> <number_of_measurements>
 
+ITER=100000000  # number of loop iterations
 results=$1".dat"
 echo "x y ey" > $results # create header
-make
+make toTable
+make $1
 for i in {1,4,8,12} # number of threads
 do
 	measurements=$i"_"$1".log" 
 	export OMP_NUM_THREADS=$i
 	for j in $(seq 1 $2) # repeat measurement $2 times
 	do
-		./$1 100000000 >> $measurements # store measurement results in <executable>.log
+		./$1 $ITER >> $measurements # store measurement results in <executable>.log
 	done
 	./toTable $measurements $results $2 $i #store table in <executable>.dat
 	rm $measurements
