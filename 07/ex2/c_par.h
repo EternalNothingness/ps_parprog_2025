33f528ff57193b02eb54bd01d6c18a82198e3ab7
Patrick Wintner <p.wintner@aon.at>
2025-05-05 15:00:47 +0200

    updated report and s07/ex2/c_par.h

--- 07/ex2/c_par.h
+++ 07/ex2/c_par.h
@@ -2,10 +2,14 @@
 #define C_PAR_H
 
 x[0] = x[0] + 5 * y[0];
+#pragma omp parallel for schedule(static)
 for (int i = 1; i<n; i++) {
 	x[i] = x[i] + 5 * y[i];
-	if ( twice ) {
+}
+
+if (twice ) {
+	#pragma omp parallel for schedule(static)
+	for (int i = 1; i<n; i++) {
 		x[i-1] = 2 * x[i-1];
-	}
 }
 #endif
